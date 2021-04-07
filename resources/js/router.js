d4a9fa41acdf1445d8d377a80249784658b7d403
tanhongit <phuongtan12357nguyen@gmail.com>
2021-04-07 16:45:59 +0700

    create example route

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,11 +3,26 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
-const routes = [];
+import ExampleComponent from "./components/ExampleComponent";
+
+const routes = [
+    {
+        /*
+         |--------------------------------------------------------------------------
+         | Admin Backend Routes
+         |--------------------------------------------------------------------------|
+         */
+         path: '/home',
+         component: ExampleComponent, // Change the desired Layout here
+         meta: { requiresAuth: true },
+         children: []
+    },
+
+];
 
 const router = new VueRouter({
     routes,
-    mode: 'history',
+    mode: 'history', //https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
     linkActiveClass: 'active'
 });
 
