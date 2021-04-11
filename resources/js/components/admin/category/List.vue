89d42aa568a8258e133570702765bc36c39d93df
tanhongit <phuongtan12357nguyen@gmail.com>
2021-04-11 20:19:00 +0700

    create new category component

---
+++ resources/js/components/admin/category/List.vue
@@ -0,0 +1,20 @@
+<template></template>
+<script>
+export default {
+  name: "List",
+  data() {
+    return {
+      categoryItem: [],
+    };
+  },
+  methods: {},
+  computed: {
+    getallCategory() {
+      return this.$store.getters.getCategory;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("allCategory");
+  },
+};
+</script>
---
+++ resources/js/components/admin/category/New.vue
@@ -0,0 +1,24 @@
+<template></template>
+<script>
+export default {
+  name: "New",
+  data() {
+    return {
+      form: new Form({
+        cat_name: "",
+        cat_description: "",
+      }),
+    };
+  },
+  methods: {
+    addCategory() {
+      this.form
+        .post("/add-category")
+        .then((response) => {
+          this.$router.push("/category-list");
+        })
+        .catch(() => {});
+    },
+  },
+};
+</script>
