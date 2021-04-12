9149adf8657aaeed38be59d4a175089ed6d3138a
tanhongit <phuongtan12357nguyen@gmail.com>
2021-04-12 22:31:48 +0700

    create partials view

---
+++ resources/views/layouts/partials/style.blade.php
@@ -0,0 +1,3 @@
+<link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/material-dashboard.minf066.css?v=2.1.0') }}" rel="stylesheet">
+<link href="{{ asset('assets/demo/demo.css') }}" rel="stylesheet">
